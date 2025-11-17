4fafdc394d27ce37fa3fb672301ebb11897e0f7c
phyowaikyaw-flutterdev <andrewtechstudio@gmail.com>
2025-11-17 22:07:11 +0700

    commit chapter 5 lessons deep diving dart

---
+++ Dart/Chapter-5/1_Introduction.dart
@@ -0,0 +1,29 @@
+void main() {
+  var name = "Mg Mg";
+}
+
+/* - Data structures are ways to organize and store data efficiently. Dart has built-in collections:
+   - Variable can hold only single value.
+   - We need techniques that can hold more than one value So Let's use Data Structure.
+   - Dart data structures are built-in language features, not a library or framework.
+   - Dart Data Structure -> can call
+     Built-in Collections / Core Collections
+     Part of Dart's core language
+     Available without importing anything
+   - Dart Collections Library (for advanced features)
+     When you need extra functionality, import dart:collection:
+
+  Key Distinctions:
+
+  Term              What It Means                       Example
+  Built-in        Part of core language               List, Map, Set
+  Library         Importable package of code         dart:collection, dart:math
+  Framework       Complete architecture/structure    Flutter (uses Dart)
+
+  - Dart has built-in data structures
+  - Dart's core collections
+  - Using the dart:collection library for advanced structures
+
+   - Data structures like List, Map, Set are built-in language features.
+     The dart:collection is a library that extends these with more specialized structures.
+*/
---
+++ Dart/Chapter-5/2_List_Initialization.dart
@@ -0,0 +1,11 @@
+void main() {
+  var names = ["Mg Mg", 20, "Aung Aung", 21, "Hong Hong", 22];
+  print(names.length);
+  print(names[3]);
+}
+
+/* 
+  List Initialization / Declaration Method.
+  Element Count => List.length
+  index => Start from 0 index
+  */
